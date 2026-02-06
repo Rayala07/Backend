768df0b65133b9d9453f3c735bf4812df07f9262
Rayala07 <rayala07viswanath@Gmail.com>
2026-02-06 19:09:15 +0530

    Generate JWT_Token and Save in Cookies

--- Authentication/Backend/src/models/user.model.js
+++ Authentication/Backend/src/models/user.model.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: String,
-  passwor: String,
+  email: {
+    type: String,
+    unique: [true, "Email already exists"],
+  },
+  password: String,
 });
 
 const userModel = mongoose.model("users", userSchema);
--- Authentication/Backend/src/routes/auth.routes.js
+++ Authentication/Backend/src/routes/auth.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const userModel = require("../../src/models/user.model");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // When you want to create api in any other file other than app.js then you have to use this express.Router(); function to build routes in individual files
 const authRouter = express.Router();
@@ -7,11 +9,30 @@ const authRouter = express.Router();
 authRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
+  const existingUser = await userModel.findOne({ email });
+
+  if (existingUser) {
+    return res.status(409).json({
+      message: "email already exists",
+    });
+  }
+
   const user = await userModel.create({ name, email, password });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
 
   res.status(201).json({
     message: "User create success",
     user,
+    token,
   });
 });
 
